9b088aa2bb0b10cdb134807c5338c526cff15d15
MaksLew <lewandowski1maksymilian@gmail.com>
2023-12-12 20:59:51 +0100

    Updated faded_colors_vector function to return string

--- src/presentation_functions.py
+++ src/presentation_functions.py
@@ -1,9 +1,11 @@
 def faded_color_vectors(initial_color, n):
     step_size = 1.0 / n
     faded_red_vectors = [
-        (round(max(0, initial_color[0] - i * step_size), 2),
-         round(max(0, initial_color[1] - i * step_size), 2),
-         round(max(0, initial_color[2] - i * step_size), 2))
+        (
+            f'rgb({round(max(0, initial_color[0] - i * step_size), 2)},'
+            f'{round(max(0, initial_color[1] - i * step_size), 2)},'
+            f'{round(max(0, initial_color[2] - i * step_size), 2)})'
+        )
         for i in range(n)
     ]
     return faded_red_vectors
