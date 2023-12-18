cfaccc108a9699fbfc901117232274929c62d1a4
MaksLew <lewandowski1maksymilian@gmail.com>
2023-12-18 08:26:58 +0100

    Updated faded_colors_vector function to work with matplotlib

--- src/presentation_functions.py
+++ src/presentation_functions.py
@@ -2,9 +2,9 @@ def faded_color_vectors(initial_color, n):
     step_size = 1.0 / n
     faded_red_vectors = [
         (
-            f'rgb({round(max(0, initial_color[0] - i * step_size), 2)},'
-            f'{round(max(0, initial_color[1] - i * step_size), 2)},'
-            f'{round(max(0, initial_color[2] - i * step_size), 2)})'
+            (round(max(0, initial_color[0] - i * step_size), 2),
+            round(max(0, initial_color[1] - i * step_size), 2),
+            round(max(0, initial_color[2] - i * step_size), 2))
         )
         for i in range(n)
     ]
